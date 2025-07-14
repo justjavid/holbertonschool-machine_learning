3fb74749d50818703d7789ddb20700f6d2e81c04
justjavid <cavidagazad163@gmail.com>
2025-07-14 16:15:37 +0000

    Add transpose function

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,29 @@
+"""
+This module provides a function to compute the transpose of a matrix.
+"""
+
+def matrix_transpose(matrix):
+    """
+    Computes the transpose of a 2D matrix.
+
+    The transpose of a matrix is obtained by flipping it over its diagonal,
+    switching the row and column indices of the elements.
+
+    Assumes all rows are of equal length.
+
+    Args:
+        matrix (list of list of numbers): A 2D matrix to transpose.
+
+    Returns:
+        list of list of numbers: The transposed matrix.
+    
+    """
+    transpose = []
+    for i in range(len(matrix[0])):
+        transpose.append([])
+    j = 0
+    while(len(transpose[len(transpose) - 1]) != len(matrix)):
+        for i in range(len(matrix)):
+            transpose[j].append(matrix[i][j])
+        j += 1
+    return transpose
