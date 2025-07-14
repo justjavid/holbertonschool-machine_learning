741ac86d5820aa92c261490adcabc6a6dfe9091c
justjavid <cavidagazad163@gmail.com>
2025-07-14 15:25:18 +0000

    Add matrix shape function

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    """
+    Returns the shape of a matrix as a list of integers.
+
+    Args:
+        matrix (list): A list of lists (possibly nested) representing the matrix.
+
+    Returns:
+        list: A list of integers representing the shape of the matrix.
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
