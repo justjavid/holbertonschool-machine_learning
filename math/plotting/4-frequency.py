0f1b4b47c077bb5d9b6e07fd3587eb2595cfaca7
justjavid <cavidagazad163@gmail.com>
2025-08-06 11:05:43 +0000

    Fix errors

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -11,5 +11,8 @@ def frequency():
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.xticks([0, 10,20,30,40,50,60,70,80,90,100])
+    plt.ylim(0, 30)
+    plt.xlim(0, 100)
     plt.hist(range = (0, 100), bins = 10, x = student_grades, edgecolor = 'black')
     plt.show()
