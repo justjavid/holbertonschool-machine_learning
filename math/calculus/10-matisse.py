01ce44703f29d293b0f4579765314ef5746ec1cf
justjavid <cavidagazad163@gmail.com>
2025-08-21 15:44:45 +0400

    Func to find polynomials' derivatives

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,8 @@
+def poly_derivative(poly):
+    if not isinstance(poly, list) or \
+    not all(isinstance(x, int) for x in poly):
+        return None
+    poly.remove(poly[0])
+    for x in range(len(poly)):
+        poly[x] = poly[x] * (x + 1)
+    return poly
