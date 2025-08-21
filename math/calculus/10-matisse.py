39b7c36dec0761e02f116a9f96f6ad435ec1391e
justjavid <cavidagazad163@gmail.com>
2025-08-21 15:46:38 +0400

    Fix

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -3,7 +3,10 @@ def poly_derivative(poly):
     if not isinstance(poly, list) or \
     not all(isinstance(x, int) for x in poly):
         return None
+    if len(poly) == 1:
+        return [0]
     poly.remove(poly[0])
     for x in range(len(poly)):
         poly[x] = poly[x] * (x + 1)
     return poly
+
