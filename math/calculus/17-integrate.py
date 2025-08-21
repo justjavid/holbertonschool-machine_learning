394d611360d096799ada90e6a6060e0acacfe98e
justjavid <cavidagazad163@gmail.com>
2025-08-21 16:33:18 +0400

    fix

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -3,6 +3,8 @@ def poly_integral(poly, C=0):
     if not isinstance(poly, list) or \
     not all(isinstance(x, int) for x in poly) or poly == []:
         return None
+    if poly == [0]:
+        return [C]
     poly.insert(0, C)
     for x in range(1, len(poly)):
         poly[x] = poly[x] / x
